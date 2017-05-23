a2739893618b05ebb2de67ff2e14e3ad3f84be78
owner <owner@owner-PC>
2017-05-23 15:55:36 +0900

    ㅌ[ㅔ스트

--- AnnMovie_prj/src/kr/co/sist/movie/run/AnnMovie.java
+++ AnnMovie_prj/src/kr/co/sist/movie/run/AnnMovie.java
@@ -3,9 +3,9 @@ package kr.co.sist.movie.run;
 import kr.co.sist.movie.view.MainView;
 
 public class AnnMovie {
-  
+   
 	public static void main(String[] args) {
-		new MainView();  // 이것을 올릴것이에요
+		new MainView();  // 이것을 올릴것이에요 1!!!!!!!
 	}
 
 }
